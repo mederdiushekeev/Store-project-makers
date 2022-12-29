e5c70ff0613b35eb1a0f6ce60c1616e867599e0a
Alibek <musicchannel0p@gmail.com>
2022-12-29 16:58:22 +0600

    added component PageRoutes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import GlobalPage from "./components/GlobalPage/GlobalPage";
+import PageRoutes from "./components/GlobalPage/PageRoutes";
 
 const App = () => {
   return (
